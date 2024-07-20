03ab8c50766dd506e401a78f9e3ef9a456af933a
dalalbinhamoud <dalalbinhamoud@gmail.com>
2024-07-20 18:02:38 +0300

    add gitignore file

---
+++ CarPricePrediction/CarPricePredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPricePredictionApp.swift
+//  CarPricePrediction
+//
+//  Created by Dalal Macbook on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarPricePredictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
